039d1e42ae03627d7c424155c4339fc8fa10005e
Lior Chamla <lchamla@gmail.com>
2016-04-08 03:05:21 +0000

    reverting et replacing all

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
 /**
- * FreeCodeCamp Request header parser microservice
- * Just returns a JSON with IP, language and OS from the browser
+ * FreeCodeCamp Timestamp microservice
+ * Receive a string parameter and gives a JSON with unix and natural date format
  * @author Lior Chamla
  */
 var http = require('http');
 var path = require('path');
 var express = require('express');
+var strftime = require('strftime');
 var router = express();
 var server = http.createServer(router);
 
+// static files (html, css ...)
+router.use(express.static(path.resolve(__dirname, 'client')));
+
 // route on GET with a parameter we call :date
-router.get('/', function(req, res){
+router.get('/:date', function(req, res){
+  // creating a date object
+  var date = new Date();
+  // if the given parameter is a number (timestamp)
+  if(/^\d*$/.test(req.params.date)){
+    date.setTime(req.params.date);
+  } 
+  // else we just create a new date parsing the string given
+  else {
+    date = new Date(req.params.date);
+  }
+  
   // giving headers for JSON
   res.set({ 'Content-Type': 'application/json' }) 
-  res.send(JSON.stringify({
-    ipaddress: req.headers['x-forwarded-for'],
-    language: req.headers['accept-language'].substring(0,5),
-    // this is the only one which is tricky : we have to extract the only information we need, for this I use a regular expression
-    software: req.headers['user-agent'].match(/\(([^(]*)\)/gi)[0].replace(/\(|\)/gi, '')
+  // if the date is invalid
+  if(!date.getTime()) res.send(JSON.stringify({error: "Invalid date given"}))
+  // else, we send the object with two members (unix and natural)
+  else res.send(JSON.stringify({
+    unix: date.getTime(),
+    natural: strftime('%B %d, %Y', date)
   }))
 })
 
 // listening to port and processing
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Request header parser microservice listening at", addr.address + ":" + addr.port);
+  console.log("Timestamp microservice listening at", addr.address + ":" + addr.port);
 });
